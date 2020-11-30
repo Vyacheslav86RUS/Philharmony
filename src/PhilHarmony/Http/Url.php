84113ad00c8d076f50cf54528a8f88da6ac41396
Vyacheslav86RUS <ProSlavon86@gmail.com>
2020-11-30 17:47:11 +0400

    fix cs

--- src/PhilHarmony/Http/Url.php
+++ src/PhilHarmony/Http/Url.php
@@ -7,9 +7,9 @@ class Url
     public static function createUrl(): string
     {
         $protocol = strpos(strtolower($_SERVER['SERVER_PROTOCOL']), 'https') === FALSE ? 'http' : 'https';
-        $hostame     = $_SERVER['HTTP_HOST'];
-        $script   = $_SERVER['SCRIPT_NAME'];
-        $params   = $_SERVER['QUERY_STRING'];
+        $hostame = $_SERVER['HTTP_HOST'];
+        $script = $_SERVER['SCRIPT_NAME'];
+        $params = $_SERVER['QUERY_STRING'];
         $currentUrl = $protocol . '://' . $hostame . $script . '?' . $params;
 
         return $currentUrl;
